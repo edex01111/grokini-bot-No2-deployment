6c6622bb5d2b50b46b07cc4d2346c9ca7b3b404a
edex01111 <edexitz51@gmail.com>
2026-09-10 14:13:17 +0100

    Update snipe.js

--- snipe.js
+++ snipe.js
@@ -1409,7 +1409,7 @@ async function showMainMenu(ctx, edit = false) {
     }
     
     const message = `
-🚀 *Welcome Maestro Sniper Bot* 🤖
+🚀 *Welcome fomo Sniper Bot* 🤖
 
 *I'm your Web3 execution engine*.
 ━━━━━━━━━━━━━━━━━━
